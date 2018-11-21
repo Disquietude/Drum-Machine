d22851d70ba666e02567b65ae121f64cd65e5f62
Disquietude <danny93ngo@gmail.com>
2018-11-21 03:46:19 -0500

    Added drum-pads to pad-container

--- src/js/components/DrumMachine.js
+++ src/js/components/DrumMachine.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Display from './Display';
 import DrumPad from './DrumPad';
 
+import sounds from '../sounds';
+
 export default class DrumMachine extends Component {
   constructor(props) {
     super(props);
@@ -10,9 +12,20 @@ export default class DrumMachine extends Component {
     };
   }
   render() {
+    const pads = sounds.map((current, index) => 
+      <DrumPad 
+        label={current.label}
+        src={current.src}
+        keybind={current.key}
+        key={index}
+      />
+    )
     return (
       <div className='drum-machine' id='drum-machine'>
         <Display />
+        <div className='drum-machine__pad-container'>
+          {pads}
+        </div>
       </div>
     );
   }
--- src/js/components/DrumPad.js
+++ src/js/components/DrumPad.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DrumPad = (props) => (
   <div className='drum-pad'>
-  
+    <audio className='clip' id={props.keybind} src={props.src}/>
   </div>
 );
 
