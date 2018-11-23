ed3dcd4050095bb64f1a2d00a28c1eb651ec5a57
Disquietude <danny93ngo@gmail.com>
2018-11-23 04:31:11 -0500

    Added 'keypress' event listeners

--- src/js/components/DrumPad.js
+++ src/js/components/DrumPad.js
@@ -1,9 +1,37 @@
 import React from 'react';
 
-const DrumPad = (props) => (
-  <div className='drum-pad'>
-    <audio className='clip' id={props.keybind} src={props.src}/>
-  </div>
-);
+const DrumPad = (props) => {
+  let audioClip = React.createRef();
+
+  let playClip = () => {
+    audioClip.current.currentTime = 0;
+    audioClip.current.play();
+  }
+
+  let handleKeyPress = (event) => {
+    if (event.key.toUpperCase() === props.keybind) {
+      playClip();
+      console.log('Pad ' + event.key.toUpperCase());
+    }
+  }
+
+  document.addEventListener('keypress', handleKeyPress);
+  
+  return (
+    <div 
+      className='drum-pad'
+      id={props.label} 
+      onClick={playClip}
+    >
+      {props.keybind}
+      <audio 
+        className='clip' 
+        id={props.keybind} 
+        src={props.src}
+        ref={audioClip}
+      />
+    </div>
+  );
+};
 
 export default DrumPad;
