dfad6448d8b83a6d2c2892033d4f708d7a643b6d
Disquietude <danny93ngo@gmail.com>
2018-11-27 05:11:12 -0500

    Updates

---
+++ precache-manifest.ae34f0de364bd2dd867eef615e110225.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "718a487579312423d66f4d242586d76c",
+    "url": "/drum-machine/static/media/kick.718a4875.wav"
+  },
+  {
+    "revision": "055271daa405d5b2f59c",
+    "url": "/drum-machine/static/css/main.b4c6ef0e.chunk.css"
+  },
+  {
+    "revision": "900df9956b9fc9c99f7c",
+    "url": "/drum-machine/static/js/1.900df995.chunk.js"
+  },
+  {
+    "revision": "3399999d14c61ad309c6",
+    "url": "/drum-machine/static/js/runtime~main.3399999d.js"
+  },
+  {
+    "revision": "7a9156f3dad569037f36b658d52f0622",
+    "url": "/drum-machine/static/media/boom.7a9156f3.wav"
+  },
+  {
+    "revision": "bb46e654060257f6fdb331aa95154a18",
+    "url": "/drum-machine/static/media/clap.bb46e654.wav"
+  },
+  {
+    "revision": "f418b88a3e18b7748bdc3a4353cae399",
+    "url": "/drum-machine/static/media/hihat.f418b88a.wav"
+  },
+  {
+    "revision": "055271daa405d5b2f59c",
+    "url": "/drum-machine/static/js/main.055271da.chunk.js"
+  },
+  {
+    "revision": "d0849ea57e591200271bca71749eedec",
+    "url": "/drum-machine/static/media/openhat.d0849ea5.wav"
+  },
+  {
+    "revision": "ccea7bb68351c2872f03c7885aa2d423",
+    "url": "/drum-machine/static/media/ride.ccea7bb6.wav"
+  },
+  {
+    "revision": "359969fcfa2a78cfa3254b17d2e23817",
+    "url": "/drum-machine/static/media/snare.359969fc.wav"
+  },
+  {
+    "revision": "bd92d16cd3995cf2aae26961e0cd9c97",
+    "url": "/drum-machine/static/media/tink.bd92d16c.wav"
+  },
+  {
+    "revision": "9813273f6b724de907291775ac7248dd",
+    "url": "/drum-machine/static/media/tom.9813273f.wav"
+  },
+  {
+    "revision": "3a52c7dc52fe274b4ab8ac4db63a2b53",
+    "url": "/drum-machine/index.html"
+  }
+];
